7f6175d6c6d4b264f1b12e737d1cf2306c33ecb5
bwan <moris09@naver.com>
2023-07-20 23:35:04 +0900

    init

---
+++ src/main/java/com/bwan/copliot/CopliotApplication.java
@@ -0,0 +1,13 @@
+package com.bwan.copliot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopliotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopliotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwan/copliot/CopliotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwan.copliot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopliotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
